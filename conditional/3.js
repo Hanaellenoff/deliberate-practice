8dbea9871d7b244988f079bdacfc02dc12a975d6
Hanaellenoff <hanaellenoff@gmail.com>
2024-04-15 20:11:58 -0400

    deliberate practice

---
+++ conditional/3.js
@@ -0,0 +1,9 @@
+// Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+var hello = 0;
+var world = 12;
+if (hello < 10 && world < 10) {
+  console.log(1);
+} else {
+  console.log(0);
+}
