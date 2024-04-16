b49ae198936d88b4fd4521d0efc0ccba56e167af
Hanaellenoff <hanaellenoff@gmail.com>
2024-04-15 20:08:32 -0400

    deliberate practice

---
+++ conditional/3.rb
@@ -0,0 +1,8 @@
+# Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+hello = 1
+world = 1
+if hello < 10 && world < 10
+  p 1
+else 
+  p 0
+end 
