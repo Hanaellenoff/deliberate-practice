2457849637acb15465fcc34ace49afc8259e7385
Hanaellenoff <hanaellenoff@gmail.com>
2024-05-16 11:57:09 -0500

    array

---
+++ arrays and hashes/411.js
@@ -0,0 +1,6 @@
+// Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+
+var array = ["hello", "world", "qwerty"];
+array.push("asdf");
+array.push("amanda");
+console.log(array);
