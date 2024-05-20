8369b26ad6b0378f85107accf9d1db9fd0cafe19
Hanaellenoff <hanaellenoff@gmail.com>
2024-05-20 11:40:10 -0500

    hashes

---
+++ arrays and hashes/422.js
@@ -0,0 +1,9 @@
+// Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+
+var qwerty = [
+  { name: "jane", last: "jose" },
+  { name: "harold", last: "smith" },
+  { name: "al", last: "green" },
+];
+
+console.log(qwerty[0]);
