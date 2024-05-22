094af7f9d6c2a031ea8364e332af9b5c6cd346c8
Hanaellenoff <hanaellenoff@gmail.com>
2024-05-22 17:32:10 -0500

    arrays

---
+++ arrays and hashes/4110.js
@@ -0,0 +1,4 @@
+// Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+var a = ["America", "Brazil", "Ireland"];
+a.push("Canada");
+console.log(a);
---
+++ arrays and hashes/4110.rb
@@ -0,0 +1,6 @@
+# Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+
+a = ["America", "Brazil", "Ireland"]
+
+a << "Canada"
+p a 
