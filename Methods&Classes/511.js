54056695f749a19ca984043f8fa645973adaf0b0
Hanaellenoff <hanaellenoff@gmail.com>
2024-05-24 14:05:40 -0500

    methods

---
+++ Methods&Classes/511.js
@@ -0,0 +1,7 @@
+// # Write a method that takes in a number and returns the number times two. Then run the method and print the result.
+
+function getDoubled(number) {
+  return number * 2;
+}
+
+console.log(getDoubled(4));
---
+++ Methods&Classes/511.rb
@@ -0,0 +1,9 @@
+# Write a method that takes in a number and returns the number times two. Then run the method and print the result.
+
+number = 5
+def intake(number)
+  times = number * 2
+  return times
+end 
+
+p intake(number)
