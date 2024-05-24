018556073f13739854d2beef1067e6f20b8e66fe
Hanaellenoff <hanaellenoff@gmail.com>
2024-05-24 14:10:38 -0500

    methods

---
+++ Methods&Classes/512.js
@@ -0,0 +1,7 @@
+// Write a method that takes in a string and returns the string with all capital letters. Then run the method and print the result.
+
+function toCapitol(string) {
+  return string.toUpperCase();
+}
+
+console.log(toCapitol("meow"));
---
+++ Methods&Classes/512.rb
@@ -0,0 +1,9 @@
+# Write a method that takes in a string and returns the string with all capital letters. Then run the method and print the result.
+
+string = "halo"
+
+def loud(string)
+  return string.upcase
+end
+
+p loud(string)
