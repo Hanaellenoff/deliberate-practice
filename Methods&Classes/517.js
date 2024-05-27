8a876d8a78e355897223ffb7dda358a77d4ae0f2
Hanaellenoff <hanaellenoff@gmail.com>
2024-05-27 11:35:57 -0500

    methods

---
+++ Methods&Classes/517.js
@@ -0,0 +1,6 @@
+// Write a method that takes in a number and returns the number as a string. Then run the method and print the result.
+
+function flip(num) {
+  return num.toString();
+}
+console.log(flip(2));
---
+++ Methods&Classes/517.rb
@@ -0,0 +1,9 @@
+# Write a method that takes in a number and returns the number as a string. Then run the method and print the result.
+
+var = 4
+
+def flip(var)
+  return var.to_s
+end 
+
+p flip(var)
