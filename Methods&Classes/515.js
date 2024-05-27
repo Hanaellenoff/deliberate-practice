e97df4fbabc34d9c4f4439d47b5cb745a0a660bd
Hanaellenoff <hanaellenoff@gmail.com>
2024-05-27 11:27:19 -0500

    methods

---
+++ Methods&Classes/515.js
@@ -0,0 +1,7 @@
+// Write a method that takes in a string and returns the first letter of the string. Then run the method and print the result.
+
+function first(string) {
+  return string[0];
+}
+
+console.log(first("hana"));
---
+++ Methods&Classes/515.rb
@@ -0,0 +1,8 @@
+# Write a method that takes in a string and returns the first letter of the string. Then run the method and print the result.
+string = "hana"
+
+def first(string)
+  return string[0]
+end 
+
+p first(string)
