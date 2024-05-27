627424af0f5d54273d1b0b138b55c921afeb1033
Hanaellenoff <hanaellenoff@gmail.com>
2024-05-27 11:52:43 -0500

    methods

---
+++ Methods&Classes/518.js
@@ -0,0 +1,8 @@
+// # Write a method that takes in a string and returns the string repeated 5 times. Then run the method and print the result.
+
+function repeat(string) {
+  for (let i = 0; i < 5; i++) {
+    console.log(string);
+  }
+}
+repeat("nasa");
---
+++ Methods&Classes/518.rb
@@ -0,0 +1,9 @@
+# Write a method that takes in a string and returns the string repeated 5 times. Then run the method and print the result.
+
+def repeat(string)
+  5.times do
+    p string
+  end 
+end
+
+repeat("nasa")
